5a960a4016c689bd61a47561f1da58dc4f37c698
snowshine09 <greenwood_sonia@yahoo.com>
2016-06-01 11:37:18 -0400

    schema fixed

--- public/assets/js/problems.js
+++ public/assets/js/problems.js
@@ -30,7 +30,7 @@
             success:function(sdata){
                 console.log('suc');
                 for (var i= 0;i<sdata.length; i++){
-                    $('tr.'+sdata[i]['problem_id']+' > .schema-list').innerHTML=10;
+                    $('tr.'+sdata[i]['problem_id']+' .schema-list')[0].innerHTML=sdata[i]['schema_count'];
                 }            
             }
         })
