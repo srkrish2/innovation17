7e2e882c0613421fa8edc62a7cfc1b17043a4f2c
Anton Kuznetsov <anton.kuznetsov@live.com>
2016-06-03 15:36:51 -0400

    java source

--- java source/InspirationHITResults copy.java
+++ java source/InspirationHITResults copy.java
@@ -1,4 +1,3 @@
-
 import java.util.Calendar;
 import java.util.List;
 
@@ -9,28 +8,28 @@ import com.amazonaws.mturk.requester.AssignmentStatus;
 import com.amazonaws.mturk.service.axis.RequesterService;
 import com.amazonaws.mturk.util.PropertiesClientConfig;
 
-public class SchemaMakingResults {
-		
+public class InspirationHITResults {
+	
 	public static void getResults(String hitId) {
 		try {
 			RequesterService service = new RequesterService(new PropertiesClientConfig());
 			
 //			output format:
 //		    # "SUCCESS"
-//		    # "--[ANSWER START]--"
+//			#  assignments_count
 //			#  assignmentId
-//		    #  worker_id
-//		    #  epoch_time_ms
-//		    #  answer
-//		    # "--[ANSWER END]--"
-//		    # "--[END]--"
+//			#  worker_id
+//			#  epoch_time_ms
+//			    # "--[ANSWER START]--"
+//			    #  answer
+//			    # "--[ANSWER END]--"
 			
 			// get submitted assignments - basically answers to the HIT
-			Assignment[] assignments;
-				assignments = service.getAllAssignmentsForHIT(hitId);
+			Assignment[] assignments = service.getAllAssignmentsForHIT(hitId);
 			
 			System.out.println("SUCCESS");
-		    
+			
+			System.out.println(assignments.length);
 			for (Assignment assignment : assignments) {
 		    	if (assignment.getAssignmentStatus() == AssignmentStatus.Submitted) {
 		    		//By default, answers are specified in XML
@@ -41,20 +40,20 @@ public class SchemaMakingResults {
 			        @SuppressWarnings("unchecked") // amazon sample code does casting here, suppressing the warning
 					List<QuestionFormAnswersType.AnswerType> answers =
 			        	(List<QuestionFormAnswersType.AnswerType>) qfa.getAnswer();
-			        QuestionFormAnswersType.AnswerType answer = answers.get(2);
-			        
-		        	String assignmentId = assignment.getAssignmentId();
-			        String answerValue = RequesterService.getAnswerValue(assignmentId, answer);
-			        System.out.println("--[ANSWER START]--");
+			        String assignmentId = assignment.getAssignmentId();
 			        System.out.println(assignment.getWorkerId());
 			        Calendar submit_time = assignment.getSubmitTime();
 			        long epoch_time_ms = submit_time.getTimeInMillis();
 			        System.out.println(epoch_time_ms);
-			        System.out.println(answerValue);
-			        System.out.println("--[ANSWER END]--");
+			        for (int i = 0; i < 3; i++) {
+			        	QuestionFormAnswersType.AnswerType answer = answers.get(i);
+				        String answerValue = RequesterService.getAnswerValue(assignmentId, answer);
+				        System.out.println("--[ANSWER START]--");
+				        System.out.println(answerValue);
+				        System.out.println("--[ANSWER END]--");
+			        }
 		    	}
 		    }
-		    System.out.println("--[END]--");
 		} catch (Exception e) {
 			System.out.println("FAIL");
 			System.out.println(e.getLocalizedMessage());
@@ -62,10 +61,8 @@ public class SchemaMakingResults {
 	}
 	
 	public static void main(String[] args) {
-		String hitId = "3MDWE879UH2R1XKLIKNLM4TO23U9B2";
-		// String hitId = args[0];
-		SchemaMakingResults.getResults(hitId);
+	    // String hitId = "3ECKRY5B1QWGUWG4SS91LFZUCX7IZ6";
+		String hitId = args[0];
+	    InspirationHITResults.getResults(hitId);
 	}
 }
-
-
