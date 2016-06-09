468efb44eb11b80fa14fa09b18cbd944a913867f
snowshine09 <greenwood_sonia@yahoo.com>
2016-06-09 16:32:14 -0400

    fixed accept or reject for schemas

--- public/assets/js/inspirations.js
+++ public/assets/js/inspirations.js
@@ -19,10 +19,38 @@
 			error: function(e){
 				console.log("error! "+e);
 			}
-		})
-		
+		})	
 	});
 	$(document).on('click','.cancelidea',function(e){
 		$('.ui.modal').modal('hide');
+	});
+	$(document).on('click','.rj',function(e){
+		var itemType = "schema", toreject=$(e.currentTarget).hasClass('red'), item_id = $(e.currentTarget.parentElement.parentElement).attr('class').split('-')[2];//if red exist, that means currently is accepted, now being hit-> changing to rej
+
+		$.ajax({
+			type: 'POST',
+			url: '/post_reject',
+			contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify({
+				"to_reject": toreject,
+				"type": itemType,
+				"id": item_id
+			}),
+			
+			success: function(sdata){
+				if(toreject){
+					$(e.currentTarget).removeClass('red');
+					$(e.currentTarget).html('Accept');
+				}
+				else {
+					$(e.currentTarget).addClass('red');
+					$(e.currentTarget).html('Reject');
+				}
+				console.log('rejected schema id '+item_id);
+			},
+			error: function(e){
+				console.log('error! ' + e);
+			}
+		});
 	});
 }(window));
--- public/assets/js/schemas.js
+++ public/assets/js/schemas.js
@@ -25,4 +25,36 @@
 	$(document).on('click','.cancelinspiration',function(e){
 		$('.ui.modal').modal('hide');
 	});
+	//reject or accept schema items
+	$(document).on('click','.ui.button.rj',function(e){
+		var itemType = "schema", toreject=$(e.currentTarget).hasClass('red'), item_id = $(e.currentTarget.parentElement.parentElement).attr('class').split('-')[2];//if red exist, that means currently is accepted, now being hit-> changing to rej
+
+		$.ajax({
+			type: 'POST',
+			url: '/post_reject',
+			contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify({
+				"to_reject": toreject,
+				"type": itemType,
+				"id": item_id
+			}),
+			
+			success: function(sdata){
+				if(toreject){
+					$(e.currentTarget).removeClass('red');
+					$(e.currentTarget).html('Accept');
+					$(e.currentTarget.parentElement.parentElement).addClass('rj');
+				}
+				else {
+					$(e.currentTarget).addClass('red');
+					$(e.currentTarget).html('Reject');
+					$(e.currentTarget.parentElement.parentElement).removeClass('rj');
+				}
+				console.log('rejected schema id '+item_id);
+			},
+			error: function(e){
+				console.log('error! ' + e);
+			}
+		});
+	});
 }(window));
