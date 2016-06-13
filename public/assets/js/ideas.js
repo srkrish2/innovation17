ed7aa30ad32c1f8a4f55ff007e526a7fb0326eb5
snowshine09 <greenwood_sonia@yahoo.com>
2016-06-13 11:53:53 -0400

    suggestion_count

--- public/assets/js/ideas.js
+++ public/assets/js/ideas.js
@@ -10,19 +10,19 @@
 		for (var i = 0; i<feedbacks.length; i++){
 			feedbackArray.push(feedbacks[i].value);
 		}
+
 		$.ajax({
 			type: 'POST',
 			url: '/post_feedback',//per idea
 			contentType: 'application/json; charset=utf-8',
 			data: JSON.stringify({
 				'idea_id':currentIdea,//length of class list is 6
-				'feedbacks': feedbackArray,
-				'count_goal': $('.count_goal').val()
+				'count_goal': $('.count_goal').val(),
+				'feedbacks': feedbackArray
 			}),
 			success: function(sdata){
-				$('tr.'+currentIdea+' td')[4].html('<div class="ui button view"><a href='+ sdata["idea_slug"]+'><i class="icon doctor"></i>0</a></div>');
+				$('tr.'+currentIdea+' td')[4].html('<div class="ui button view"><a href='+ sdata["suggestions_page_link"]+'><i class="icon doctor"></i>0</a></div>');
 				console.log('launched suggestion seeking');
-				// window.location.replace(sdata['url']);
 			},
 			error: function(e){
 				console.log("error! "+e);
@@ -51,4 +51,17 @@
 	$(document).on('click','.ui.button.view', function(e){
 		$('<div class="field"><input type="text" class="feedback" placeholder="Enter the feedback for the idea">').insertBefore($(e.currentTarget));
 	});
+	
+
+	function makePostRequest(){
+		$.ajax({
+			type: "GET",
+			url: '/suggestion_updates',
+			success:function(sdata){
+				for (var i= 0;i<sdata.length; i++){
+					if($('tr.'+sdata[i]['idea_id']+' .suggestion-list').length)$('tr.'+sdata[i]['idea_id']+' .suggestion-list')[0].innerHTML="<i class='doctor icon'></i> "+sdata[i]['suggestion_count'];
+				}
+			}
+		})
+	}
 }(window));
